72775d7ee8d2521fba241a0fd424dc10cc21400c
Runn1ngGr1zzly <185625550+Runn1ngGr1zzly@users.noreply.github.com>
2026-09-06 14:01:49 -0700

    Adding logging to the game

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import SCREEN_WIDTH
 from constants import SCREEN_HEIGHT
+from logger import log_state
+
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
