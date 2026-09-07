ae5f95c3f6f021cd419ff53f8cc092e63930a86a
Runn1ngGr1zzly <185625550+Runn1ngGr1zzly@users.noreply.github.com>
2026-09-07 15:36:15 -0700

    Added framerate restriction

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0
 
     while True:
         log_state()
@@ -20,6 +22,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
