2502f0ba7a9b90916b3fbb0c331a5f5b6e104ff2
Runn1ngGr1zzly <185625550+Runn1ngGr1zzly@users.noreply.github.com>
2026-09-07 18:37:49 -0700

    Created spawning asteroids in asteroid field

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import SCREEN_WIDTH
 from constants import SCREEN_HEIGHT
 from logger import log_state
@@ -15,8 +17,12 @@ def main():
     dt = 0.0
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    AsteroidField()
 
 
     while True:
