4b3493d60bd1ea6bbe0e90c3a92079c380f84a21
Runn1ngGr1zzly <185625550+Runn1ngGr1zzly@users.noreply.github.com>
2026-09-07 17:02:35 -0700

    Added left and right rotation to the player object

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ def main():
     while True:
         log_state()
         screen.fill("black")
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         for event in pygame.event.get():
