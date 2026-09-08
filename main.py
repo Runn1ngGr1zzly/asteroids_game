4a37e32c6ca4bac364c3fdc339c4f7b18a3692bf
Runn1ngGr1zzly <185625550+Runn1ngGr1zzly@users.noreply.github.com>
2026-09-07 18:00:48 -0700

    started building pygame groups

--- main.py
+++ main.py
@@ -13,13 +13,18 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
 
     while True:
         log_state()
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
